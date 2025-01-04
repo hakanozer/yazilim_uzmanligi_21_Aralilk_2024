ce06f2af95a4fe4e4d11ebfa6b1bc1c1a51757d0
Hakan ÖZER <hakanozer02@gmail.com>
2025-01-04 10:25:40 +0300

    Days_4

---
+++ csharp/Days_4/Days_4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Days_4;
+
+class Program
+{
+    static void Main(String[] argas)
+    {
+        Console.WriteLine("Lütfen sayı giriniz!");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int end = number;
+        if (end == 0)
+        {
+            Console.WriteLine($"end: 1");
+        }
+        else
+        {
+            for (int i = number - 1; i > 0; i--)
+            {
+                end = end * i;
+            }
+            Console.WriteLine($"end: {end}"); ;
+        }
+    }
+}
+
+
+
