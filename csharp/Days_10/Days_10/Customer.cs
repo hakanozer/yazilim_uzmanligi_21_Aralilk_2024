509e2ced04e273b8da222d061b859b6c0292412c
Hakan ÖZER <hakanozer02@gmail.com>
2025-01-25 12:40:04 +0300

    Days_10

---
+++ csharp/Days_10/Days_10/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Days_10
+{
+	public class Customer
+	{
+
+		public void Login( string email, string password )
+		{
+			Console.WriteLine($"{email} - {password}");
+		}
+
+	}
+}
+
---
+++ csharp/Days_10/Days_10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+
+namespace Days_10
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Customer customer = new Customer();
+
+            // Sınıf içindeki özellikleri kullanmak için nesne üretimi yapmak gerekir.
+            // SınıfAdı nesneAdı = new SınıfAdı();
+            // sınıf içindeki özelliklere (.) ile erişilir.
+            Profile profile = new Profile("Erkan", "Bilirim");
+            profile.Call();
+
+
+            DB db = new DB(Edb.SQLITE);
+            db.connect();
+            db.close();
+
+            // İstisnalar
+            string stNum1 = "12";
+            string stNum2 = "77";
+
+            int num1 = Convert.ToInt32(stNum1);
+            int num2 = Convert.ToInt32(stNum2);
+
+            int sm = num1 + num2;
+            Console.WriteLine($"Sum: {sm}");
+        
+        }
+    }
+}
