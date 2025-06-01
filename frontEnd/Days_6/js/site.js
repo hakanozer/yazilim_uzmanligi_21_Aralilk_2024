acecfb90ebaa577a987ce93ebfa541dd040c2efd
Hakan ÖZER <hakanozer02@gmail.com>
2025-06-01 13:45:08 -0700

    Days_6

--- frontEnd/Days_6/js/site.js
+++ frontEnd/Days_6/js/site.js
@@ -38,14 +38,37 @@ function getNameVal() {
     console.log(nameVal, titleVal)
 }
 
+function nameTextChange(evt) {
+    //const nameObj = document.getElementById('name')
+    //const name = nameObj.value
+    //console.log(name)
+    const name = evt.value
+    if (name.toLowerCase() == 'erkan') {
+        pullList()
+    }else {
+        listReset()
+    }
+
+    // <i class="bi bi-1-square fs-4"></i>
+    const users = ["ali", "veli", "erkan", "zehra"]
+    const iconObj = document.getElementById('icon')
+    const index = users.findIndex(item => item.toLowerCase() == name.toLowerCase())
+    if (index > -1) {
+        iconObj.innerHTML = '<i class="bi bi-'+index+'-square"></i>'
+    }else {
+       iconObj.innerHTML = ''
+    }
+}
+
 function pullList() {
     var html = ''
     var liHtml = ''
-    const citiesArr = ['Ankara', 'İstanbul', 'İzmir', 'Bursa', 'Antalya', 'Adana']
+    var selectCity = 'İstanbul'
+    const citiesArr = ['Ankara', selectCity, 'İzmir', 'Bursa', 'Antalya', 'Adana']
     for (let i = 0; i < citiesArr.length; i++) {
         const item = citiesArr[i];
-        const selected = item == 'İstanbul' ? 'selected': ''
-        const active = item == 'İstanbul' ? 'active': ''
+        const selected = item == selectCity ? 'selected': ''
+        const active = item == selectCity ? 'active': ''
         html += '<option '+selected+' value="'+i+'">'+item+'</option>'
         liHtml += '<li class="list-group-item '+active+'">'+item+'</li>'
     }
@@ -53,4 +76,11 @@ function pullList() {
     const ulCitiesObj = document.getElementById('ulCities')
     citiesObj.innerHTML = html
     ulCitiesObj.innerHTML = liHtml
+}
+
+function listReset() {
+    const citiesObj = document.getElementById('cities');
+    const ulCitiesObj = document.getElementById('ulCities')
+    citiesObj.innerHTML = ''
+    ulCitiesObj.innerHTML = ''
 }
