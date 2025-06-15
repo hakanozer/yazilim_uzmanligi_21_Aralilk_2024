40691e4a2a8680b483063fbae774f5c30a28168b
Hakan ÖZER <hakanozer02@gmail.com>
2025-06-15 11:12:39 +0300

    Object Table

--- frontEnd/Days_6/js/site.js
+++ frontEnd/Days_6/js/site.js
@@ -76,6 +76,7 @@ function pullList() {
     const ulCitiesObj = document.getElementById('ulCities')
     citiesObj.innerHTML = html
     ulCitiesObj.innerHTML = liHtml
+    citiesObj.style.backgroundColor = 'lightblue'
 }
 
 function listReset() {
@@ -84,3 +85,54 @@ function listReset() {
     citiesObj.innerHTML = ''
     ulCitiesObj.innerHTML = ''
 }
+
+//object
+function getTable() {
+    const user1 = {
+        id: 100,
+        name: 'Ali',
+        surname: 'Bilmem',
+        email: 'ali@mail.com',
+        status: true,
+        colors: [
+            "red", "black", "green"
+        ],
+        address: {
+            city: 'İstanbul',
+            area: 'Marmara',
+            zip: 34400
+        }
+    }
+    const arr = dataResult()
+    const newArr = arr.sort((a, b) => a.id - b.id)
+    var html = ''
+    for (let i = 0; i < newArr.length; i++) {
+        const item = newArr[i];
+        html += `
+        <tr>
+            <th scope="row">`+item.id+`</th>
+            <td>`+item.name+`</td>
+            <td>`+item.surname+`</td>
+            <td>`+item.status+`</td>
+            <td>`+item.age+`</td>
+        </tr>`
+    }
+    const tableObj = document.getElementById('tableData')
+    tableObj.innerHTML = html    
+}
+
+function dataResult() {
+    var arr = []
+    for (let i = 0; i < 10; i++) {
+        const obj = {
+            id: Math.round(Math.random() * 100),
+            name: 'Name-'+i,
+            surname: 'Surname-'+i,
+            status: i %2 === 0 ? true : false,
+            age: i
+        }
+        arr.push(obj) // dizi içine veri ekle
+    }
+    return arr
+}
+getTable()
