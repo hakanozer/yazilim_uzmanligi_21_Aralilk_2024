441cede6e12ac8a9b236f689c2997b3d53fe1705
Hakan ÖZER <hakanozer02@gmail.com>
2025-06-21 12:24:42 +0300

    Days_8

--- frontEnd/Days_8/js/site.js
+++ frontEnd/Days_8/js/site.js
@@ -4,10 +4,34 @@ $(document).ready(function(){
     $('button').click(function() {
         //alert('Button Submit')
     })
-    // 2. seçici türü: id' göre
+    // 2. seçici türü: id'ye göre
     $('#submit').click(function() {
-        alert('Button Submit Call')
+        //alert('Button Submit Call')
     })
+    // 3. seçici türü: class'a göre
+    $('.form-label').click(function(){
+        alert('Form Label Click')
+    })
+    // 4. seçici türü: nesne içindeki özelliğe göre(.,#)
+    $('div.bg-danger').click(function(){
+        alert('Div Click')
+    })
+    // 5. this seçici türü: hangi nesne seçili ise onun nesnesi üzerinden işlem yap
+    $('div#box').click(function(){
+        //$(this).css('background-color', 'red') // bg-secondary
+        $(this).toggleClass('bg-danger')
+    })
+
+    // form submit
+    $('#loginForm').submit(function (e) {
+        e.preventDefault();// formun sahneyi terk etmesini engeller.
+        const email = $('#email').val()
+        const password = $('#password').val()
+        const remember = $('#remember').is(':checked')
+        const city = $('#city').val()
+        console.log(email, password, remember, city)
+    });
+
 })
 
 // seçici (.) yetenek (.) function(){  }
