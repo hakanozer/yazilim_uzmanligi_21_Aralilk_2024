90a06e4ada712a4eda2e0aa347eddefab022a354
Hakan ÖZER <hakanozer02@gmail.com>
2025-06-21 10:02:47 +0300

    Days_8

--- frontEnd/Days_8/js/site.js
+++ frontEnd/Days_8/js/site.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    
+    // Seçiciler
+    // 1. seçici türü: nesneye göre
+    $('button').click(function() {
+        //alert('Button Submit')
+    })
+    // 2. seçici türü: id' göre
+    $('#submit').click(function() {
+        alert('Button Submit Call')
+    })
 })
 
 // seçici (.) yetenek (.) function(){  }
