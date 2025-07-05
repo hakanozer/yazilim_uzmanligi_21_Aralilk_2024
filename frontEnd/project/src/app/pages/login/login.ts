48753acf17539b4bad5bf96c564e0507ac3c80f7
Hakan ÖZER <hakanozer02@gmail.com>
2025-07-05 11:19:33 +0300

    Login Form

--- frontEnd/project/src/app/pages/login/login.ts
+++ frontEnd/project/src/app/pages/login/login.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { Bar } from '../../components/bar/bar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [Bar],
+  imports: [Bar, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
-  
+  // user models
+  email = ''
+  password = ''
+
+  // fonksion
+  userLogin(){
+    console.log("Data:", this.email, this.password)
+  }
+
 
 }
