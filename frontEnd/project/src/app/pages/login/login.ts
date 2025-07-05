641991fba5e833e570f8548aec52059efa2f9056
Hakan ÖZER <hakanozer02@gmail.com>
2025-07-05 10:22:03 +0300

    Bar

--- frontEnd/project/src/app/pages/login/login.ts
+++ frontEnd/project/src/app/pages/login/login.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Bar } from '../../components/bar/bar';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Bar],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  
 
 }
--- frontEnd/project/src/app/pages/register/register.ts
+++ frontEnd/project/src/app/pages/register/register.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Bar } from '../../components/bar/bar';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [Bar],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
