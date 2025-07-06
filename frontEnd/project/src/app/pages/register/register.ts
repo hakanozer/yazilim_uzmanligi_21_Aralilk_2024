05b1a16b5e591ce48c07038bead5fb5f93a09916
Hakan ÖZER <hakanozer02@gmail.com>
2025-07-06 11:10:59 +0300

    Register

--- frontEnd/project/src/app/pages/register/register.ts
+++ frontEnd/project/src/app/pages/register/register.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { Bar } from '../../components/bar/bar';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [Bar, FormsModule],
+  imports: [Bar, FormsModule, RouterModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
 
   passlock = false
+  passType = "password"
+
+  // register values
+  name = ''
+  email = ''
+  password = ''
+  passwordAgain = ''
 
   // register fnc
   userRegister() {
@@ -20,6 +28,7 @@ export class Register {
   // password text lock and unlock
   passwordLockUnLock() {
     this.passlock = !this.passlock
+    this.passType = this.passlock === true ? 'text' : 'password'
   }
 
 }
