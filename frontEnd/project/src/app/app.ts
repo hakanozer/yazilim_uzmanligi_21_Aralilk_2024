ecdc49bc10ae88c772b3fd0d5fd0870b7af1d05d
Hakan ÖZER <hakanozer02@gmail.com>
2025-08-02 12:26:19 +0300

    Update app.ts

--- frontEnd/project/src/app/app.ts
+++ frontEnd/project/src/app/app.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Api } from './services/api';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class App {
   protected title = 'project';
-  tokenStatus = localStorage.getItem('token') ? true : false
+  tokenStatus = false
+  constructor(private api: Api, private cdr: ChangeDetectorRef) {
+    this.authControl()
+  }
+
+  async authControl() {
+    try {
+      const res = await this.api.userProfileSync()
+      if (res) {
+        this.tokenStatus = true
+        this.cdr.detectChanges();
+      }
+    } catch (error) {
+      this.tokenStatus = false
+      this.cdr.detectChanges();
+    }
+  }
+
 }
