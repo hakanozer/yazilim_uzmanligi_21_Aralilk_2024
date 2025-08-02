909acd14c0b43eb52e125c8b828fd74e3cf34bb4
Ahmet_Demircan <57825302+code0-lab@users.noreply.github.com>
2025-08-02 09:36:48 +0300

    Revert "Odev2"
    This reverts commit 827197d785ad94148a0496524f2a171367564d36.

--- odevler/angular/OdevTeslim/Odev1/Ufuk_Login/Ufuk_Login/src/app/utils/apiUrl.ts
+++
@@ -1,5 +0,0 @@
-const baseUrl = "https://dummyjson.com/auth/"
-
-export const authUrl = {
-    login: `${baseUrl}login`
-}
--- odevler/angular/OdevTeslim/Odev2/AhmetDemircan_Odev2/Ahmet_Login_Odev1/src/app/utils/valids.ts
+++
@@ -1,7 +0,0 @@
-export const firstCharUpper = (item:string) : string => {
-    item = item.toLocaleLowerCase('tr')
-    const first = item[0].toLocaleUpperCase('tr')
-    item = item.substring(1, item.length)
-    item = first+item
-    return item
-}
