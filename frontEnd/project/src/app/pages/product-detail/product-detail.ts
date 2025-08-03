a2e76ed187c2fa5894aa638de037056394aa7056
Hakan ÖZER <hakanozer02@gmail.com>
2025-08-03 12:17:18 +0300

    Detail

--- frontEnd/project/src/app/pages/product-detail/product-detail.ts
+++ frontEnd/project/src/app/pages/product-detail/product-detail.ts
@@ -14,6 +14,8 @@ export class ProductDetail {
 
   product: Product | null = null
   globalPrice = ''
+  stars:number[] = []
+  bigImage = ''
 
   constructor(private route: ActivatedRoute, private api: Api, private router: Router, private cdr: ChangeDetectorRef){
     this.route.params.subscribe(params => {
@@ -24,6 +26,9 @@ export class ProductDetail {
           next: (value) => {
             this.product = value.data
             this.globalPrice = (value.data.price + ((value.data.price * value.data.discountPercentage) / 100)).toFixed(2)
+            this.countStars(value.data.rating)
+            this.bigImage = value.data.images[0]
+            this.cdr.detectChanges();
           },
           error: (err) => {
             alert("Not found product: " + id)
@@ -38,6 +43,23 @@ export class ProductDetail {
       }
       
     })
+  }
+
+  countStars(rating: number) {
+    const arr:number[] = []
+    const tamSayi = Math.floor(rating)
+    const yarimSayi = Math.ceil(rating - tamSayi)
+    const bosSayi = 5 - (tamSayi + yarimSayi)
+    for (let i = 0; i < tamSayi; i++) {
+      arr.push(1)
+    }
+    if (yarimSayi > 0) {
+      arr.push(0.5)
+    }
+    for (let i = 0; i < bosSayi; i++) {
+      arr.push(0)
+    }
+    this.stars = arr
   }
 
 }
