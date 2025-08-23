f8ceaebe9d3b872ccba5b00be983f6cbd1456fde
Hakan ÖZER <hakanozer02@gmail.com>
2025-08-23 10:58:41 +0300

    Page

--- frontEnd/project/src/app/pages/products/products.ts
+++ frontEnd/project/src/app/pages/products/products.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@
 import { Api } from '../../services/api';
 import { Pagination, Product } from '../../models/IProducts';
 import { ProductItem } from '../../components/product-item/product-item';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-products',
-  imports: [ProductItem],
+  imports: [ProductItem, RouterModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
   changeDetection: ChangeDetectionStrategy.Default
@@ -20,15 +21,33 @@ export class Products implements OnInit {
     total_pages: 0
   }
   pages: number[] = []
+  current_page = 1
 
-  constructor( private api: Api, private cdr: ChangeDetectorRef ){
-  }
+  constructor( private api: Api, 
+    private cdr: ChangeDetectorRef, 
+    private activeRouter: ActivatedRoute)
+    {}
+
+
+
 
   ngOnInit(): void {
-    this.api.allProducts(1, 10).subscribe({
+    this.activeRouter.queryParams.forEach((params) => {
+      const page = params['page']
+      if (page) {
+        this.current_page = page
+        this.pullData()
+      }
+    })
+    this.pullData()
+  }
+
+  pullData() {
+      this.api.allProducts(this.current_page, 10).subscribe({
       next: (value) => {
         this.productArr = value.data
         this.pageInfo = value.meta.pagination
+        this.pages = []
         for (let i = 0; i < value.meta.pagination.total_pages; i++) {
           this.pages.push(i+1)
         }
