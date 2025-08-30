c4a0b5e94ee4ea244163a7ce0e3a378be9ea29f6
Hakan ÖZER <hakanozer02@gmail.com>
2025-08-30 11:47:40 +0300

    Nodemon - End

--- nodejs/dist/app.js
+++ nodejs/dist/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
+// get - mvc
+app.get('/data', (req, res) => {
+    res.status(200).json({ message: 'Hello, World!!!' });
+});
 app.listen(PORT, () => {
     console.log(`Server running: http://localhost:${PORT}`);
 });
--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -3,6 +3,10 @@ import express from 'express'
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.get('/data', (req, res) => {
+  res.status(200).json({ message: 'Hello, World!-' })
+})
+
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`)
 })
