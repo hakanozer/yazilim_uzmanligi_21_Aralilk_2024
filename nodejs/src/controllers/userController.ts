ed8f4d2431ea5a42719b6db113bd3aba4b4911a9
Hakan ÖZER <hakanozer02@gmail.com>
2025-08-30 13:57:37 +0300

    Model

--- nodejs/src/controllers/userController.ts
+++ nodejs/src/controllers/userController.ts
@@ -2,7 +2,20 @@ import express  from "express"
 
 export const userController = express.Router()
 
+const userObj = {
+    id: 150,
+    name: 'Erkan',
+    surname: 'Bilsin'
+}
+const arr = [
+    "İstanbul", "Ankara", "İzmir", "Bursa"
+]
 // userLogin
 userController.get("/", (req, res) => {
-    res.render('login')
+    res.render('login', {
+        title: 'User Loginx', 
+        cat: 100, 
+        user: userObj,
+        cities: arr
+    })
 })
