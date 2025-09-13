399b5c72ebe94e03ff604d605927fdffbdcc5ccd
tubanursmsk <tubanursmsk@gmail.com>
2025-09-13 09:04:12 +0300

    check

--- odevler/nodejs/ödev_2/Teslim/TubaSimsek/node.js/src/services/dashboardService.ts
+++ odevler/nodejs/ödev_2/Teslim/TubaSimsek/node.js/src/services/dashboardService.ts
@@ -2,21 +2,21 @@ import DashboardDB, { IDashboard } from "../models/dashboardModel";
 
 export const addNote = async (userId: string, title: string, detail: string, date?: Date, color?: string) => {
   try {
-    const newNote = new DashboardDB({ userId, title, detail, date, color });
-    await newNote.save();
-    return true;
+    const newNote = new DashboardDB({ userId, title, detail, date, color })
+    await newNote.save()
+    return true
   } catch (err) {
-    console.error("addNote error:", err);
-    return false;
+    console.error("addNote error:", err)
+    return false
   }
 };
 
 export const getUserNotes = async (userId: string) => {
   try {
-    const notes = await DashboardDB.find({ userId }).sort({ date: -1 });
-    return notes;
+    const notes = await DashboardDB.find({ userId }).sort({ date: -1 })
+    return notes
   } catch (err) {
-    console.error("getUserNotes error:", err);
-    return [];
+    console.error("getUserNotes error:", err)
+    return []
   }
 };
