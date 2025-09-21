4bf4a68f331f8e46da0892536ce700e018597a0f
Hakan ÖZER <hakanozer02@gmail.com>
2025-09-21 13:55:12 +0300

    Config

--- nodejs/RestApi/src/app.ts
+++ nodejs/RestApi/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import path from 'path'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-
+import { connectDB } from './configs/db';
 
 // .env Config - .env file loading
 dotenv.config({path: path.resolve(__dirname, '../.env')});
@@ -10,12 +10,16 @@ dotenv.config({path: path.resolve(__dirname, '../.env')});
 const app = express()
 const PORT = process.env.PORT || 4000
 
+// DB Config
+connectDB()
+
 // body-parser Config
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 // import Rest Controllers
 import userRestController from './restcontrollers/userRestController';
+
 
 // Routers Config
 app.use('/api/users', userRestController)
