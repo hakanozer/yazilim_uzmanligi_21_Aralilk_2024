1fc94f1bef75d899f368c041b24cea45cf6f2854
Ahmet_Demircan <57825302+code0-lab@users.noreply.github.com>
2025-09-27 10:10:01 +0300

    BugFixed
    Redirect 404 fixed

--- bitirme_projeleri/angular/AhmetDemircan/LudusMagnus/src/app/Pages/enroll-Course/enroll-Course.ts
+++ bitirme_projeleri/angular/AhmetDemircan/LudusMagnus/src/app/Pages/enroll-Course/enroll-Course.ts
@@ -200,7 +200,7 @@ export class EnrollCourse implements OnInit {
 
   // Kurslar sayfasına geri dön
   goBack(): void {
-    this.router.navigate(['/courses']); // Kurslar sayfasına yönlendir
+    this.router.navigate(['/Courses']); // Kurslar sayfasına yönlendir
   }
 }
 
