42826f0e13b9a5c51b2bd31025a255e4744dd4e8
Ahmet_Demircan <57825302+code0-lab@users.noreply.github.com>
2025-09-30 11:46:51 +0300

    Odev_5
    app.ts değişimleri eklendi, commentRestController tamamlandı

---
+++ odevler/nodejs/ödev_5/Yorum yazma/AhmetDemircan/app.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import { connectDB } from './configs/db';
+
+// .env Config - .env file loading
+dotenv.config({path: path.resolve(__dirname, '../.env')});
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+// DB Config
+connectDB()
+
+// body-parser Config
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// import Rest Controllers
+import userRestController from './restcontrollers/userRestController';
+import categoryRestController from './restcontrollers/categoryRestController';
+import commentRestController from './restcontrollers/commentRestController'; //eklendi (Ahmet Demircan)
+
+
+
+// Routers Config
+app.use('/api/v1/users', userRestController)
+app.use('/api/v1/categories', categoryRestController)
+app.use('/api/v1/comments', commentRestController) //eklendi (Ahmet Demircan)
+
+
+app.listen(PORT, () => {
+  console.log(`Server running: http://localhost:${PORT}`)
+})
+
