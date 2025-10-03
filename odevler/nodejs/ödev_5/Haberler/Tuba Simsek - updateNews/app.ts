1b350af786c31312d8b11b141142eb31030cf873
tubanursmsk <tubanursmsk@gmail.com>
2025-10-03 22:31:45 +0300

    updateNews

---
+++ odevler/nodejs/ödev_5/Haberler/Tuba Simsek - updateNews/app.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import { connectDB } from './configs/db';
+
+// .env Config - .env file loading
+dotenv.config({path: path.resolve(__dirname, '../.env')});
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+// DB Config
+connectDB()
+
+// body-parser Config
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// import Rest Controllers
+import userRestController from './restcontrollers/userRestController';
+import newsRestController from './restcontrollers/newsRestController';
+
+
+// Routers Config
+app.use('/api/v1/users', userRestController)
+app.use('/api/v1/news', newsRestController)
+
+
+app.listen(PORT, () => {
+  console.log(`Server running: http://localhost:${PORT}`)
+})
+
