9dbd7a112d85bef7555039ff5a670a9a9cf7e891
Hakan ÖZER <hakanozer02@gmail.com>
2025-10-04 10:30:17 +0300

    Update category.ts

--- nodejs/RestApi/src/models/category.ts
+++ nodejs/RestApi/src/models/category.ts
@@ -1,29 +1,38 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
-export interface ICategory {
-  findOne(arg0: { name: { $regex: RegExp; }; }): unknown;
-  id: number;
-  name: string;
-  description?: string;
-  isactive: boolean;
-  createdAt: Date;
-  updatedAt: Date;
+export interface IComment extends Document {
+    content: string;
+    createdAt: Date; //otomatik çekilecek
+    updatedAt: Date; //otomatik çekilecek
+    lastUpdatedById: mongoose.Types.ObjectId; //en son yorumu düzenleyenn id si tutulacak
+    userId: mongoose.Types.ObjectId; // Yorumu yazanın id si
+    newsId: mongoose.Types.ObjectId; // Yorumun ait olduğu haberin id si
+    isActive: boolean; //bu değer admin tarafından yorumu aktif yapmak için kullanılır
+    like: number;
+    dislike: number;
 }
 
-const CategorySchema: Schema<ICategory> = new Schema(
-  {
-    id: { type: Number, required: true },
-    name: { type: String, required: true, minlength: 2, maxlength: 50 },
-    description: { type: String, required: false, trim: true, maxlength: 200 },
-    isactive: { type: Boolean, required: true },
+const commentSchema = new Schema<IComment>({
+    content: { type: String, required: true, minlength: 20, maxlength: 250 }, //Yorum için max ve min standardı eklendi
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
-  },
-  {
-    timestamps: true,
-  }
-);
+    lastUpdatedById: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    isActive: { type: Boolean, default: false },
+    like: { type: Number, default: 0 },
+    dislike: { type: Number, default: 0 },
+    newsId: { type: Schema.Types.ObjectId, ref: "News", required: true },
+});
 
-const Category = mongoose.model<ICategory>('Category', CategorySchema);
+//Otomatik tarih alımı ara katmanı (veri tabanına kayıttan önce uğranan katman, kanca)
+commentSchema.pre("save", function (next) { // save olmadan önce çalışması için ayarlı "pre save"
+    if (this.isModified() && !this.isNew) {
+        this.updatedAt = new Date();
+    }
+    next();
+});
 
-export default Category;
+const CommentDB = mongoose.model<IComment>('Comment', commentSchema); // Comment modeli oluşturuldu
+
+export default CommentDB; // dışa aktarıldı (kullanıma hazır)
+
