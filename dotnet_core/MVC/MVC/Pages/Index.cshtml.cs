480a660a1921e355b789a0e16527d45b601b398a
Hakan ÖZER <hakanozer02@gmail.com>
2025-11-15 12:15:23 +0300

    Form Post

--- dotnet_core/MVC/MVC/Pages/Index.cshtml.cs
+++ dotnet_core/MVC/MVC/Pages/Index.cshtml.cs
@@ -5,8 +5,24 @@ namespace MVC.Pages
 {
     public class IndexModel : PageModel
     {
+
+        [BindProperty]
+        public string Username { get; set; } = string.Empty;
+
+        [BindProperty]
+         public string Password { get; set; } = string.Empty;
+
         public void OnGet()
         {
+            Console.WriteLine("Razor Pages Login GET");
         }
+
+        public IActionResult OnPost()
+        {
+            Console.WriteLine("Razor Pages Login POST");
+            Console.WriteLine(Username);
+            return Page();
+        }
+
     }
 }
